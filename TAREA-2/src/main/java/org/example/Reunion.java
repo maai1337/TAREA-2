4c11db8cd04fdf44878c03ecd66f8633d14ce6b3
Felipegarciah <fegarcia2024@udec.cl>
2025-05-16 19:54:12 -0400

    se hicieron cambios en rounion con respecto al tiempo falta arreglar hora prevista

--- TAREA-2/src/main/java/org/example/Reunion.java
+++ TAREA-2/src/main/java/org/example/Reunion.java
@@ -1,87 +1,55 @@
 package org.example;
-import java.time.Duration;
-import java.time.Instant;
+import java.time.*;
 import java.util.Date;
 import java.util.List;
 
 
 abstract public class Reunion {
-    private Date fecha;
+    private LocalDate fecha;
     private Instant horaPrevista;
     private Duration duracionPrevista;
     private Instant horaInicio;
     private Instant horaFin;
 
-    public List<Empleado> obtenerAsistencias(){
-        return null;
-    }
 
-    public List<Empleado> obtenerAusencias(){
-        return null;
-    }
+    public Reunion(int hora, int minuto, int minutos_reunion) {
+        this.fecha = LocalDate.now();
+        this.duracionPrevista = Duration.ofMinutes(minutos_reunion);
 
-    public List<Empleado> obtenerRetrasos(){
-        return null;
-    }
+        /**
+         * debo ver como colocar la hora a hora chile como instant trbaja con utc
+         * o dejarlo asi nada mas
+         * */
 
-    public int obtenerTotalAsistencias(){
-        return 0;
-    }
+        ZoneId horaCompu = ZoneId.systemDefault();
+        LocalDateTime fechahora =LocalDateTime.of(this.fecha, LocalTime.of(hora,minuto));
+        Instant horaPrevista = ZonedDateTime.of(LocalDateTime.now(), horaCompu).toInstant(hora,minuto);
+        ZonedDateTime zonedDateTime = horaPrevista.atZone(zonaCompu);
+        this.horaPrevista = zonedDateTime.toInstant();
+        // ver como arreglar esto
 
-    public float obtenerPorcentajeAsistencias(){
-        return 0;
-    }
-
-    public float obtenerTiempoReal(){
-        return 0;
     }
 
     public void iniciar(){
         this.horaInicio = Instant.now();
     }
-
     public void finalizar(){
         this.horaFin = Instant.now();
     }
 
-    public Instant getHoraInicio(){
-        return this.horaInicio;
+    public Instant getHoraPrevista() {
+        return horaPrevista;
+
     }
 
-    public Instant getHoraFin(){
-        return this.horaFin;
+    public float calcularTiempoReal(){
+        if (this.horaInicio != null && this.horaFin != null) {
+            Duration duracion = Duration.between(this.horaInicio, this.horaFin);
+            return duracion.toSeconds() / 60f;
+        }else {
+            return 0;
+        }
     }
 
-    public Duration getDuracionPrevista(){
-        return this.duracionPrevista;
-    }
-
-    public Instant getHoraPrevista(){
-        return this.horaPrevista;
-    }
-
-    public Date getFecha(){
-        return this.fecha;
-    }
-
-    public void setFecha(Date fecha){
-        this.fecha = fecha;
-    }
-
-    public void setHoraPrevista(Instant horaPrevista){
-        this.horaPrevista = horaPrevista;
-    }
-
-    public void setDuracionPrevista(Duration duracionPrevista){
-        this.duracionPrevista = duracionPrevista;
-    }
-
-    public void setHoraInicio(Instant horaInicio){
-        this.horaInicio = horaInicio;
-    }
-
-    public void setHoraFin(Instant horaFin){
-        this.horaFin = horaFin;
-    }
 
 }
