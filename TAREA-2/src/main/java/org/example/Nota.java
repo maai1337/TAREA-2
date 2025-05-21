b3a05fa9289f4591b53448e1a78ab351610b171e
Felipegarciah <fegarcia2024@udec.cl>
2025-05-20 22:47:46 -0400

    funcion de guardar momento en clase nota

--- TAREA-2/src/main/java/org/example/Nota.java
+++ TAREA-2/src/main/java/org/example/Nota.java
@@ -1,13 +1,19 @@
 package org.example;
 
+import java.time.Instant;
+
 public class Nota {
 
     private String Contenido;
-
+    private Instant minuto;
     public Nota(String contenido) {
         this.Contenido = contenido;
+        this.minuto = Instant.now();
     }
     public String getContenido() {
         return Contenido;
+    }
+    public String getMinuto() {
+        return minuto.toString();
     }
 }
